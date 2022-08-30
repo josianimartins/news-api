f0f356288e7e768b0e655ace474e9a8c4ef596c9
Josiani Martins <35438620+josianimartins@users.noreply.github.com>
2022-08-30 17:19:46 -0300

    Removing Article import in App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-import Article from "./components/Article";
-
 import ArticleList from "./components/ArticleList";
 
 export default function App() {
